44fdda94410dbdfe28c8fbbc251e12e1534762c8
MurashovS <imurasov0@gmail.com>
2024-01-25 16:51:11 +0300

    add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,4 +19,14 @@ public class StatisticsServiceTest {
 
     }
 
+    @Test
+    void findMaxFromMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 80, 6, 11, 11, 12};
+        long expected = 80;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
